06ef0d52d887ac9fd106c24aef89474c983e056f
Anton Gridushko <gridushko.anton@yahoo.com>
2024-02-26 06:16:14 +0300

    Add migration.

---
+++ Rss/rss-api/Migrations/20240226001449_Initial.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NpgsqlTypes;
+
+#nullable disable
+
+namespace rss_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RssElements",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Tag = table.Column<string>(type: "text", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RssElements", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RssFeeds",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    RssDalElementsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Tag = table.Column<string>(type: "text", nullable: true),
+                    Header = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    Description = table.Column<string>(type: "character varying(30000)", maxLength: 30000, nullable: true),
+                    SearchVector = table.Column<NpgsqlTsVector>(type: "tsvector", nullable: true),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RssFeeds", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RssFeeds_RssElements_RssDalElementsId",
+                        column: x => x.RssDalElementsId,
+                        principalTable: "RssElements",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RssFeeds_Id",
+                table: "RssFeeds",
+                column: "Id")
+                .Annotation("Npgsql:IndexInclude", new[] { "Header" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RssFeeds_RssDalElementsId",
+                table: "RssFeeds",
+                column: "RssDalElementsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RssFeeds");
+
+            migrationBuilder.DropTable(
+                name: "RssElements");
+        }
+    }
+}
