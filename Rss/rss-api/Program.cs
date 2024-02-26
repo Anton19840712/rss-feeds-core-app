343e232530e118c30334f1fee7e638d32beb4832
Anton Gridushko <gridushko.anton@yahoo.com>
2024-02-26 06:18:47 +0300

    Register services.

--- Rss/rss-api/Program.cs
+++ Rss/rss-api/Program.cs
@@ -1,15 +1,57 @@
+using Hangfire;
+using Hangfire.PostgreSql;
+using Microsoft.EntityFrameworkCore;
+using rss_api.Contexts;
+using rss_api.ControllerHandlers;
+using rss_api.Mapping;
+using rss_api.Services;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// logger:
+Log.Logger = new LoggerConfiguration()
+	.Enrich.FromLogContext()
+	.WriteTo.Console()
+	.CreateLogger();
+
+builder.Services.AddMapster();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<IHttpService, HttpService>();
+builder.Services.AddScoped<ICacheService, CacheService>();
+builder.Services.AddScoped<IDataBaseService, DataBaseService>();
+
+builder.Services.AddScoped<IHangFireService, HangFireService>();
+
+builder.Services.AddHttpClient();
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true);
+
+var connectionStringPostgresql = builder.Configuration.GetConnectionString("Connection");
+
+builder.Services.AddDbContext<RssDbContext>(options =>
+{
+	options.UseNpgsql(connectionStringPostgresql);
+});
+
+// hangfire START:
+builder.Services.AddHangfire(config =>
+{
+	// Hangfire should be in a database before you connect to it.
+	var connectionString = builder.Configuration.GetConnectionString("Connection");
+	config.UsePostgreSqlStorage(connectionString);
+});
+builder.Services.AddHangfireServer();
+// hangfire END:
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// AddMapster the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
